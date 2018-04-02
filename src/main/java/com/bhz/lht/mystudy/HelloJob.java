b7e5e8959ed281490f8b6e11d611f81c83e0779a
刘洪天 <liuht@bhz.com.cn>
2018-04-02 15:25:45 +0800

    修改输出信息

--- src/main/java/com/bhz/lht/mystudy/HelloJob.java
+++ src/main/java/com/bhz/lht/mystudy/HelloJob.java
@@ -10,7 +10,7 @@ public class HelloJob implements Job {
 
 	@Override
 	public void execute(JobExecutionContext context) throws JobExecutionException {
-		System.out.println("my first job running " + runningCount++ + "!");
+		System.out.println("my job running " + runningCount++ + "!");
 
 	}
 
