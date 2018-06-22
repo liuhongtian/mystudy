2317ecadd0f50531602e7869edc6828b22eae645
刘洪天 <liuht@bhz.com.cn>
2018-06-22 09:02:54 +0800

    a

--- src/main/java/com/bhz/lht/mystudy/App.java
+++ src/main/java/com/bhz/lht/mystudy/App.java
@@ -1,11 +1,13 @@
 package com.bhz.lht.mystudy;
 
+import java.util.UUID;
+
 /**
  * Hello world!
  *
  */
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println(UUID.randomUUID().toString().replaceAll("-", ""));
 	}
 }
