91c22847606418726a883902343f86acf99f50cc
刘洪天 <liuht@bhz.com.cn>
2018-09-13 16:33:22 +0800

    tst

--- src/main/java/com/bhz/lht/mystudy/FileWriter.java
+++ src/main/java/com/bhz/lht/mystudy/FileWriter.java
@@ -7,7 +7,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Arrays;
-//import java.util.UUID;
 
 public class FileWriter {
 	private static final int BUFFER_LENGTH = 100;
@@ -21,20 +20,12 @@ public class FileWriter {
 		Arrays.stream(fileNames, 0, fileNames.length).forEach(f -> {
 			try (InputStream is = new FileInputStream(new File(imgName))) {
 				File file = new File(f);
-				if (!file.exists()) {
-					try {
-						file.createNewFile();
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-				}
 				int len = 0;
 				try (OutputStream os = new FileOutputStream(file);) {
 					while ((len = is.read(buff)) != -1) {
 						System.out.println("read: " + len);
 						os.write(buff, 0, len);
 					}
-					// os.write(UUID.randomUUID().toString().getBytes());
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
@@ -43,6 +34,15 @@ public class FileWriter {
 			}
 
 		});
+
+		byte[] bytes = { (byte) 0xe6, (byte) 0xa1, (byte) 0xe6, (byte) 0xa1, (byte) 0xe6, (byte) 0xa1, (byte) 0xe6,
+				(byte) 0xa1 };
+		try (OutputStream os3 = new FileOutputStream(new File("/Users/liuhongtian/Downloads/test3"))) {
+			os3.write(bytes);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 		System.out.println("Done.");
 	}
 
