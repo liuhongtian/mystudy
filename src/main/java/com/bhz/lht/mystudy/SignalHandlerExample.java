00f4bf5c093d43f82f03e42deffdd8d95f64b3e9
刘洪天 <liuht@bhz.com.cn>
2018-11-19 14:29:35 +0800

    信号捕捉样例

---
+++ src/main/java/com/bhz/lht/mystudy/SignalHandlerExample.java
@@ -0,0 +1,27 @@
+package com.bhz.lht.mystudy;
+
+import sun.misc.Signal;
+import sun.misc.SignalHandler;
+
+public class SignalHandlerExample implements SignalHandler {
+
+	@Override
+	public void handle(Signal sig) {
+		System.out.println("receive SIGNAL: " + sig.getName());
+		System.exit(0);
+	}
+	
+	public static void main(String... args) {
+		Signal.handle(new Signal("TERM"), new SignalHandlerExample());
+		//Signal.handle(new Signal("KILL"), new SignalHandlerExample());
+		while(true) {
+			System.out.println("waitting ...");
+			try {
+				Thread.sleep(5000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+}
